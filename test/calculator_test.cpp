54a4941f600b977a0b5afb48482c169175871f86
Sergii Piatakov <sergii.piatakov@globallogic.com>
2019-02-11 01:22:06 +0200

    introduce a test fixture
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- test/calculator_test.cpp
+++ test/calculator_test.cpp
@@ -2,14 +2,45 @@
 #include "calculator.h"
 
 
-TEST (CalculatorTest, Add_Success)
+class CalculatorTest : public ::testing::Test
+{
+    public:
+        CalculatorTest  () = default;
+        ~CalculatorTest () = default;
+
+        void SetUp      () override
+        {
+            ::testing::Test::SetUp ();
+        }
+
+        void TearDown   () override
+        {
+            ::testing::Test::TearDown ();
+        }
+
+    protected:
+        Calculator      calculator;
+};
+
+TEST_F (CalculatorTest, Add_Case001_Success)
 {
     // Arrange.
-    Calculator calculator;
     const int etalon = 1;
 
     // Act.
     const int test = calculator.Add (0, 1);
+
+    // Arrange.
+    EXPECT_EQ (etalon, test);
+}
+
+TEST_F (CalculatorTest, Add_Case002_Success)
+{
+    // Arrange.
+    const int etalon = 5;
+
+    // Act.
+    const int test = calculator.Add (2, 3);
 
     // Arrange.
     EXPECT_EQ (etalon, test);
